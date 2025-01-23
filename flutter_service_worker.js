c70676691158ed3ebbd7ca0b4482b52243d947e1
Ruslan.D <x.stelvia@gmail.com>
2025-01-24 02:44:02 +0600

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7d41aa212a31d665a66c6e5b86d31bb2",
+const RESOURCES = {"flutter_bootstrap.js": "d2fd8749fbb90ad5174a459e2a12e9f0",
 "version.json": "3d2ae1201680513a199a7a8b5dbc2c48",
 "index.html": "67c88880572d8b41175f436cc647b262",
 "/": "67c88880572d8b41175f436cc647b262",
-"main.dart.js": "cd0ceada7d1f93f108d184b6447cd836",
+"main.dart.js": "90fd31e9ab3f47f7051959378771991f",
 "sqlite3.wasm": "78498ab7d022b0069cdf876adf5b1515",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
